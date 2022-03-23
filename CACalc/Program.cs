b0acc95f52dbf38c6b4186153f2444d5d2f749bd
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2022-03-23 20:25:24 -0300

    gravação inicial

--- CACalc/Program.cs
+++ CACalc/Program.cs
@@ -10,6 +10,49 @@ namespace CACalc
     {
         static void Main(string[] args)
         {
+            double valor1;
+            double valor2;
+            double resultado;
+            string op;
+
+            do
+            {
+                Console.WriteLine("Digite um valor");
+                valor1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Escolha uma operação:");
+                op = Console.ReadLine();
+                Console.WriteLine("Digite um valor");
+                valor2 = double.Parse(Console.ReadLine());
+                
+                switch (op)
+                {
+                    case "+":
+                        resultado = valor1 + valor2;
+                        break;
+                    case "-":
+                        resultado = valor1 - valor2; 
+                        break;
+                    case "*":
+                        resultado = valor1 * valor2;
+                        break;
+                    case "/":
+                        while (valor2==0)
+                        {
+                            Console.WriteLine("Opcao invalida, digite um novo valor:");
+                            valor2 = double.Parse(Console.ReadLine());
+
+                        }
+                        resultado = valor1 / valor2;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcao invalida");
+                        break;
+                }
+                Console.WriteLine("Deseja fazer uma nova operação:");
+                op = Console.ReadLine();
+            } while (!op.ToUpper().Equals("S"));
+
         }
     }
 }
